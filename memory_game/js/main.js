95138d996a46f35aca59354f2163c4f1e5f860c5
Arif Shoeb <arifshoeb@Arifs-iMac.local>
2019-09-27 11:47:45 -0400

    push up memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,27 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "King";
-var cardFour = "King";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+const cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards [0];
+cardsInPlay.push("User flipped" + cardOne);
+console.log();
+var cardTwo = cards[1];
+cardsInPlay.push("User flipped" + cardTwo);
+console.log();
+if (["queen", "queen", "king", "king"].length) === 2 {
+	console.log();
+} else if cardsInPlay[0] === cardsInPlay[1] {
+ console.log("You found a match!");
+} else {
+	console.log("Sorry, try again.");
+}
+
+
+
+
+
+
+
+
+
